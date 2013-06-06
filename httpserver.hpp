41550dd9fff6dcdfe3a7228b5234a8c5d8e83d9b
Sjors Gielen <sjors@sjorsgielen.nl>
2013-06-06 10:00:57 +0200

    Add SSL support to Skynetd.

--- httpserver.hpp
+++ httpserver.hpp
@@ -6,19 +6,36 @@
 #include <boost/thread/thread.hpp>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
+#include <memory>
 #include "http_global.hpp"
 #include "httpconnection.hpp"
 
 namespace skynet {
 
+typedef std::unique_ptr<boost::asio::ssl::context> SslContext;
+
 class HttpServer {
 public:
+	HttpServer(std::string address, std::string port,
+		size_t thread_pool_size, SslContext context,
+		HttpServerDelegatePtr delegate)
+	: io_service_()
+	, acceptor_(io_service_)
+	, thread_pool_size_(thread_pool_size)
+	, delegate_(delegate)
+	, ssl_context_(std::move(context))
+	, ssl_(true) {
+		init_acceptor(address, port);
+		start_accept();
+	}
+
 	HttpServer(std::string address, std::string port,
 		size_t thread_pool_size, HttpServerDelegatePtr delegate)
 	: io_service_()
 	, acceptor_(io_service_)
 	, thread_pool_size_(thread_pool_size)
-	, delegate_(delegate) {
+	, delegate_(delegate)
+	, ssl_(false) {
 		init_acceptor(address, port);
 		start_accept();
 	}
@@ -46,6 +63,13 @@ private:
 	typedef boost::asio::ip::tcp tcp;
 
 	void init_acceptor(std::string address, std::string port) {
+		if(ssl_ && (port == "http" || port == "80")) {
+			std::cerr << "Warning: SSL enabled, but listening on port 80, this is not recommended." << std::endl;
+		}
+		if(!ssl_ && (port == "https" || port == "443")) {
+			std::cerr << "Warning: SSL disabled, but listening on port 443, this is not recommended." << std::endl;
+		}
+
 		tcp::resolver resolver(io_service_);
 		tcp::resolver::query query(address, port);
 		tcp::endpoint endpoint = *resolver.resolve(query);
@@ -56,7 +80,12 @@ private:
 	}
 
 	void start_accept() {
-		BaseSocketPtr sock(new Socket(io_service_));
+		BaseSocketPtr sock;
+		if(ssl_) {
+			sock.reset(new SslSocket(io_service_, *ssl_context_));
+		} else {
+			sock.reset(new Socket(io_service_));
+		}
 		sock->async_accept(acceptor_,
 			[this,sock](boost::system::error_code e) {
 				if(e) {
@@ -75,6 +104,8 @@ private:
 	tcp::acceptor acceptor_;
 	size_t thread_pool_size_;
 	HttpServerDelegatePtr delegate_;
+	SslContext ssl_context_;
+	bool ssl_;
 };
 
 }
--- socket.hpp
+++ socket.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 
 namespace skynet {
 
@@ -47,6 +48,47 @@ struct Socket : public BaseSocket
 
 private:
 	tcp::socket socket_;
+};
+
+struct SslSocket : public BaseSocket
+{
+	SslSocket(boost::asio::io_service &io_service,
+	    boost::asio::ssl::context &context)
+	: socket_(io_service, context)
+	{}
+
+	void async_accept(tcp::acceptor &acceptor, std::function<void(boost::system::error_code)> f) {
+		acceptor.async_accept(socket_.lowest_layer(), f);
+	}
+
+	virtual void async_start(std::function<void()> f)
+	{
+		socket_.async_handshake(boost::asio::ssl::stream_base::server,
+			[f](boost::system::error_code error) {
+				if(error) {
+					std::cerr << "SSL handshake error: " << error << std::endl;
+				} else {
+					f();
+				}
+			}
+		);
+	}
+
+	virtual void async_read_some(boost::asio::mutable_buffers_1 b,
+		std::function<void(boost::system::error_code, size_t)> f)
+	{
+		socket_.async_read_some(b, f);
+	}
+
+	virtual void async_write(std::vector<boost::asio::const_buffer> &b,
+		std::function<void(boost::system::error_code, size_t)> f)
+	{
+		boost::asio::async_write(socket_, b, f);
+	}
+
+private:
+	typedef boost::asio::ssl::stream<boost::asio::ip::tcp::socket> ssl_socket;
+	ssl_socket socket_;
 };
 
 }
