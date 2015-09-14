baf6cc1a6656663e8f6accafb9ec6586cc9b2d0a
Nick Overdijk <nick@astrant.net>
2015-09-14 15:08:48 +0200

    Add forward.hpp, contains forward declarations

---
+++ forward.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace sg { namespace http {
+	struct ApiRoute;
+	struct ApiRouter;
+	struct HttpMessage;
+	struct HttpRequest;
+	struct HttpResponse;
+	struct IncompleteHttpMessageException;
+	struct InvalidHttpMessageException;
+	struct NoChunksLeftException;
+	struct Uri;
+}}
+
